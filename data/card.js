4eabdf8648c0e04bd67ecee10a6a1f98d16c4bf8
Tarana Aliyeva <008039@ibar.int>
2024-03-14 17:27:18 +0400

    added checkout page

--- data/card.js
+++ data/card.js
@@ -1,4 +1,10 @@
-export const card = [];
+export let card = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+},{
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+}];
 
 export  function addToCard(productId){
     let matchingItem;
@@ -18,4 +24,15 @@ export  function addToCard(productId){
         });
     }
     
+  }
+
+
+ export function removeFromCard(productId){
+    const newCard = [];
+    card.forEach((cardItem)=>{
+        if(cardItem.productId !== productId){
+            newCard.push(cardItem);
+        }
+    });
+    card = newCard;
   }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import { card, addToCard } from "../data/card.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -23,7 +24,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
@@ -75,6 +76,7 @@ document.querySelectorAll('.js-add-to-card')
     .forEach((button) => {
         button.addEventListener('click', () => {
             const {productId} = button.dataset;
+            console.log(productId);
             addToCard(productId);
             updateCardQuantity();
             
