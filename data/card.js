f4d7c053bde03442cbf7c9420b215237832ae856
Tarana Aliyeva <008039@ibar.int>
2024-03-14 17:34:09 +0400

    added localStorage

--- data/card.js
+++ data/card.js
@@ -1,10 +1,19 @@
-export let card = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
-},{
-    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-}];
+export let card = JSON.parse(localStorage.getItem('card'));
+
+if(!card){
+    card = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2
+    },{
+        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }];
+}
+
+
+function saveToStorage(){
+    localStorage.setItem('card', JSON.stringify(card))
+}
 
 export  function addToCard(productId){
     let matchingItem;
@@ -23,7 +32,7 @@ export  function addToCard(productId){
             quantity
         });
     }
-    
+    saveToStorage();
   }
 
 
@@ -35,4 +44,5 @@ export  function addToCard(productId){
         }
     });
     card = newCard;
+    saveToStorage();
   }
